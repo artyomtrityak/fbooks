15a46433d541ecb9eeba0eb730ca74596e8d79b1
Artyom Trityak <art.trityak@gmail.com>
2013-10-18 18:40:51 +0300

    Fix css positions

--- static/controllers/home/main.js
+++ static/controllers/home/main.js
@@ -1,24 +1,24 @@
 define(function(require) {
   'use strict';
 
-  var MODULE_NAME = 'HomeScreen',
+  var MODULE_NAME = 'HomeModule',
       homeTemplate = require('text!./template.html'),
       module = angular.module(MODULE_NAME, ['ngRoute']);
 
   module.config(['$routeProvider', function($routeProvider) {
       
-      $routeProvider.when('/home', {
-        template: homeTemplate,
-        controller: 'HomeScreen.home'
-      });
+    $routeProvider.when('/home', {
+      template: homeTemplate,
+      controller: MODULE_NAME+'.Home'
+    });
 
-      $routeProvider.otherwise({
-        redirectTo: '/home'
-      });
-    }]
-  );
+    $routeProvider.otherwise({
+      redirectTo: '/home'
+    });
+    
+  }]);
 
-  module.controller(MODULE_NAME+'.home', ['$scope', function ($scope) {
+  module.controller(MODULE_NAME+'.Home', ['$scope', function ($scope) {
     $scope.name = 'Artyom';
   }]);
 
